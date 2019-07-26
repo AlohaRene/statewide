121ec7922993c6482dd7e8cf684955465edfb59d
aloha-rene <reneonlineg1@gmail.com>
2019-07-26 02:56:17 -0400

    added some  js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
-class Test {
-	main = () => {
-		console.log(swag);
-	};
-}
-Test.main();
+// class Test {
+// 	main = () => {
+// 		console.log(swag);
+// 	};
+// }
+// Test.main();
+
+
+$(document).ready(function() {
+  
+  
+  
+    /*STICKY*/
+    $('.js--section-features').waypoint(function(direction) {
+      if (direction == "down") {
+        $('nav').addClass('sticky');
+        
+      } else {
+          $('nav').removeClass('sticky');
+      }
+    }, {
+    offset: '60px;'
+    });
+   
+    
+    /*Srcoll on buttons*/
+    
+    $('.js--scroll-to-plans').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /*navigation scroll*/
+    $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    })
+  
+    
+    });
